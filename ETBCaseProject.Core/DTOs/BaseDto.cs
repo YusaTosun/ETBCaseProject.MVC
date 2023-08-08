a61a2ff244e5e7c14cb9b3dbbf1de26e418fee65
Yusa Tosun <yusatosun.yt@gmail.com>
2023-08-08 17:14:34 +0300

    Service class and Intereface has implemented

---
+++ ETBCaseProject.Core/DTOs/BaseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETBCaseProject.Core.DTOs
+{
+    public class BaseDto
+    {
+        public int Id { get; set; }
+
+        //public DateTime CreatedDate { get; set; } // todo: Bunun gerekli olup olmadığına sonra karar ver
+    } 
+}
--- ETBCaseProject.Core/Models/BaseEntity.cs
+++ ETBCaseProject.Core/Models/BaseEntity.cs
@@ -8,5 +8,6 @@
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
     }
 }
